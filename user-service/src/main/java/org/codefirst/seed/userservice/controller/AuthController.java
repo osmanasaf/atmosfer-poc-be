b745631cb77a11fe7334fa5e93753c28783f7c6d
Mahmut Boğaz <mbogaz93@gmail.com>
2023-05-04 11:55:45 +0300

    user service send kafka message

--- user-service/src/main/java/org/codefirst/seed/userservice/controller/AuthController.java
+++ user-service/src/main/java/org/codefirst/seed/userservice/controller/AuthController.java
@@ -2,7 +2,9 @@ package org.codefirst.seed.userservice.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.codefirst.seed.userservice.dto.AdminExistDto;
+import org.codefirst.seed.userservice.service.KafkaService;
 import org.codefirst.seed.userservice.service.LdapService;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,7 @@ import java.util.List;
 public class AuthController {
 
     private final LdapService ldapService;
+    private final KafkaService kafkaService;
 
     @GetMapping("/admin/exist/{username}")
     public AdminExistDto isAdminExist(@PathVariable("username") String username) {
@@ -23,6 +26,8 @@ public class AuthController {
         AdminExistDto dto = new AdminExistDto();
         dto.setUsername(username);
         dto.setExist(admins.size() > 0);
+
+        kafkaService.sendMessage("isAdminExist", null, username);
         return dto;
     }
 }
